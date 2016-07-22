a6d54d9f245c504fef70d9894995a71c32f7c2d6
ryh <1971990184@qq.com>
2016-07-22 15:50:30 +0800

    add mongodump tool

--- tools/mongodump.py
+++ tools/mongodump.py
@@ -1,3 +1,26 @@
 import os
 
-os.system('ls')
+import datetime
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['stockcodes']
+documents = db.HS300.find()
+stockcodes = []
+for document in documents:
+    stockcodes.append(document['stockcode'])
+
+now_time = datetime.datetime.now()
+yes_time = now_time + datetime.timedelta(days=-5)
+grab_time = yes_time.strftime('%m-%d')
+
+os.system('mkdir '+grab_time)
+
+for stockcode in stockcodes:
+    os.system('mongodump --db '+stockcode+'eastmoney'+' --collection '+grab_time+'GuYouHui')
+    os.system('mongodump --db '+stockcode+'eastmoney'+' --collection '+grab_time+'SentimentFactor')
+    os.system('mongodump --db '+grab_time)
+
+os.system('mv dump '+grab_time)
+
+# os.system('ls')
